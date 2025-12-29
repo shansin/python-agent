76444ec1d0887e28b5d6687dee26f034c6772e24
Shantanu Singh <shansin@users.noreply.github.com>
2025-12-29 00:29:04 -0800

    deep research agent- first take

--- openai-agents-example.py
+++ openai-agents-example.py
@@ -46,7 +46,7 @@ async def simple_agent_example():
         result = await Runner.run(agent, "Tell me a joke about Autonomous AI agents")
         print(result.final_output)
 
-def send_email(to: str, sub: str, body: str, type: str) -> dict:
+def send_email_sendgrid(to: str, sub: str, body: str, type: str) -> dict:
     import sendgrid
     from sendgrid.helpers.mail import Mail, Email, To, Content
 
@@ -197,7 +197,7 @@ async def multiple_agents_as_tool_example():
 
         Crucial Rules:
         - You must use the sales agent tools to generate the drafts — do not write them yourself.
-        - You must send ONE email using the send_emailsend_cold_sales_email tool — never more than one.
+        - You must send ONE email using the send_cold_sales_email tool — never more than one.
         """
 
     sales_manager_agent = Agent(name="SalesManager", instructions=instructions, model=llama_model, tools=tools)
@@ -211,7 +211,7 @@ async def multiple_agents_as_tool_example():
 def send_cold_sales_email(body: str):
     """Sends a cold sales email to a predefined email address."""
     #above line acts as description for the tool to llm
-    send_email(to="mailme.shantanu@gmail.com", sub="Cold Sales Email", body=body, type="text/plain")
+    send_email_sendgrid(to="mailme.shantanu@gmail.com", sub="Cold Sales Email", body=body, type="text/plain")
 
 async def multiple_agents_as_tool_and_handoff_and_guardrail_example():
     client = AsyncOpenAI(base_url=ollama_base_url, api_key="ollama")
@@ -314,7 +314,7 @@ async def multiple_agents_as_tool_and_handoff_and_guardrail_example():
 @function_tool
 def send_html_email(subject: str, html_body: str) -> Dict[str, str]:
     """ Send out an email with the given subject and HTML body to all sales prospects """
-    send_email(to="mailme.shantanu@gmail.com", sub="Cold Sales Email", body=html_body, type="text/html")
+    send_email_sendgrid(to="mailme.shantanu@gmail.com", sub="Cold Sales Email", body=html_body, type="text/html")
     return {"status": "success"}
 
 
@@ -390,9 +390,141 @@ async def basic_research_agent(research: str):
         result = await Runner.run(search_agent, research)
         print(result.final_output)
 
+async def deep_research_agent(research: str, breadth: int):
+    from agents.model_settings import ModelSettings
+    from pydantic import BaseModel, Field
+    from openai import AsyncOpenAI
+    from agents.model_settings import ModelSettings
+    
+    client = AsyncOpenAI(base_url=ollama_base_url, api_key="ollama")
+    
+    gpt_model = OpenAIChatCompletionsModel(model="gpt-oss:20b", openai_client=client)
+    llama_model = OpenAIChatCompletionsModel(model="llama3.1:8b", openai_client=client)
+    deepseek_model = OpenAIChatCompletionsModel(model="deepseek-r1:8b", openai_client=client)
+
+    search_instructions = "You are a research assistant. Given a search term, you search the web with web_search_tool and \
+    produce a concise summary of the results. The summary must 2-3 paragraphs and less than 300 \
+    words. Capture the main points. Write succintly, no need to have complete sentences or good \
+    grammar. This will be consumed by someone synthesizing a report, so it's vital you capture the \
+    essence and ignore any fluff. Do not include any additional commentary other than the summary itself."
+
+    @function_tool
+    def web_search_tool(search_topic: str):
+        """Tool to search the web"""
+        #return tavily_search(search= search_topic, max_results= 10)
+        return searxng_search(search= search_topic, page_no=1)
+
+    search_agent = Agent(
+        name="Search agent",
+        instructions=search_instructions,
+        tools=[web_search_tool],
+        model=gpt_model,
+        #model_settings=ModelSettings(tool_choice="required")
+    )
+
+    planning_instructions = f"You are a helpful research assistant. Given a query, come up with a set of web searches \
+    to perform to best answer the query. Output {breadth} terms to query for."
+
+    class WebSearchItem(BaseModel):
+        reason: str = Field(description="Your reasoning for why this search is important to the query.")
+        query: str = Field(description="The search term to use for the web search.")
+
+    class WebSearchPlan(BaseModel):
+        searches: list[WebSearchItem] = Field(description="A list of web searches to perform to best answer the query.")
+
+    planner_agent = Agent(
+        name="PlannerAgent",
+        instructions=planning_instructions,
+        model=gpt_model,
+        output_type=WebSearchPlan,
+    )
+
+    email_instructions = """You are able to send a nicely formatted HTML email based on a detailed report.
+    You will be provided with a detailed report. You should use your tool to send one email, providing the 
+    report converted into clean, well presented HTML with an appropriate subject line."""
+
+    @function_tool
+    def send_email(subject: str, html_body: str) -> Dict[str, str]:
+        """ Send out an email with the given subject and HTML body """
+        send_email_sendgrid(to="mailme.shantanu@gmail.com", sub= subject, body=html_body, type="text/html")
+        return "success"
+
+    email_agent = Agent(
+        name="EmailAgent",
+        instructions=email_instructions,
+        tools=[send_email],
+        model=gpt_model
+    )
+
+    writer_instructions = (
+        "You are a senior researcher tasked with writing a cohesive report for a research query. "
+        "You will be provided with the original query, and some initial research done by a research assistant.\n"
+        "You should first come up with an outline for the report that describes the structure and "
+        "flow of the report. Then, generate the report and return that as your final output.\n"
+        "The final output should be in markdown format, and it should be lengthy and detailed. Aim "
+        "for 5-10 pages of content, at least 1000 words."
+    )
+
+    class ReportData(BaseModel):
+        short_summary: str = Field(description="A short 2-3 sentence summary of the findings.")
+        markdown_report: str = Field(description="The final report")
+        follow_up_questions: list[str] = Field(description="Suggested topics to research further")
+
+    writer_agent = Agent(
+        name="WriterAgent",
+        instructions=writer_instructions,
+        model=gpt_model,
+        output_type=ReportData,
+    )
+
+    async def plan_searches(query: str):
+        """ Use the planner_agent to plan which searches to run for the query """
+        print("Planning searches...")
+        result = await Runner.run(planner_agent, f"Query: {query}")
+        print(f"Will perform {len(result.final_output.searches)} searches")
+        return result.final_output
+
+    async def perform_searches(search_plan: WebSearchPlan):
+        """ Call search() for each item in the search plan """
+        print("Searching...")
+        tasks = [asyncio.create_task(search(item)) for item in search_plan.searches]
+        results = await asyncio.gather(*tasks)
+        print("Finished searching")
+        return results
+
+    async def search(item: WebSearchItem):
+        """ Use the search agent to run a web search for each item in the search plan """
+        input = f"Search term: {item.query}\nReason for searching: {item.reason}"
+        result = await Runner.run(search_agent, input)
+        return result.final_output
+    
+    async def write_report(query: str, search_results: list[str]):
+        """ Use the writer agent to write a report based on the search results"""
+        print("Thinking about report...")
+        input = f"Original query: {query}\nSummarized search results: {search_results}"
+        result = await Runner.run(writer_agent, input)
+        print("Finished writing report")
+        return result.final_output
+
+    async def send_email(report: ReportData):
+        """ Use the email agent to send an email with the report """
+        print("Writing email...")
+        result = await Runner.run(email_agent, report.markdown_report)
+        print("Email sent")
+        return report
+
+    with trace("Research trace"):
+        print("Starting research...")
+        search_plan = await plan_searches(research)
+        search_results = await perform_searches(search_plan)
+        report = await write_report(research, search_results)
+        await send_email(report)  
+        print("Hooray!")
+
+
 async def main():
     #push_notification("Starting OpenAI agent examples")
-    #send_email(to="mailme.shantanu@gmail.com", sub="Starting OpenAI agent examples", body="The OpenAI agent examples script has started running.")
+    #send_email_sendgrid(to="mailme.shantanu@gmail.com", sub="Starting OpenAI agent examples", body="The OpenAI agent examples script has started running.")
     #send_email_resend(to=["mailme.shantanu@gmail.com","dixit.upasanaitbhu@gmail.com"], sub="Welcome to ShanUP.com", body="Mark this date as when it started. \n<a href=\"https://www.shanup.com/\">Visit ShanUP.com!</a>", from_name="Shantanu", from_email="Shantanu@shanup.com")
     #searxng_search("News in Bothell Washington", 1)
     #tavily_search("News in Bothell Washington", 20)
@@ -402,7 +534,8 @@ async def main():
     #await multiple_agents_example()
     #await multiple_agents_as_tool_example()
     #await multiple_agents_as_tool_and_handoff_and_guardrail_example()
-    await basic_research_agent("Top Agentic AI frameworks to look forward to in 2026")
+    #await basic_research_agent("Top Agentic AI frameworks to look forward to in 2026")
+    await deep_research_agent("Top Agentic AI frameworks to look forward to in 2026", 3)
     
 
 if __name__ == "__main__":
